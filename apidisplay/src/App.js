16c0cc46c3c854854be21e32cc2de30908f4a46f
VladimirAndreev09 <Vladimir.Andreev09@myhunter.cuny.edu>
2021-04-19 20:50:00 -0400

    Created app and layout

---
+++ apidisplay/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component, useState } from 'react';
+import './App.css';
+import Newton from './components/newton';
+
+function App() {
+  return (
+    <div className="App">
+      <Newton/>   
+    </div>
+  );
+}
+export default App
